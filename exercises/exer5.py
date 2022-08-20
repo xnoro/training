6b02ac46d8afc586b5e4fdc68526989ae15f264a
xnoro <superboydbr@gmail.com>
2022-08-20 18:13:02 -0300

    added exercise 5

---
+++ exercises/exer5.py
@@ -0,0 +1,3 @@
+numero=input('Opa, vamos para a divisão, insira um número para ser dividido: ')
+divisor=input('Nice! Agora digite um número para dividir: ')
+print('Perfect, o resultado equivale à', str(int(numero)/int(divisor))+'!!!')
