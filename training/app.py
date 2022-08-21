1634822da707b6daee8c6e8692d6d044413f1094
xnoro <superboydbr@gmail.com>
2022-08-20 22:20:38 -0300

    minor changes, added folders and python files

---
+++ training/app.py
@@ -0,0 +1,14 @@
+velocidade_internet = 10
+print(velocidade_internet)
+
+
+boleanos = True
+
+strings = 'paulatejando'
+
+a,b,c,d = 1,2,3,4
+print(a, b, c, d)
+
+print(type(a))
+print(type(strings))
+
---
+++ training/test1.py
@@ -0,0 +1,9 @@
+var1 = 25.87
+var2 = True
+var3 = 'Bom dia!'
+var4 = 15
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
