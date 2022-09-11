2dab0820299f9b97c8b8d8996417247bc6aa1fe1
xnoro <superboydbr@gmail.com>
2022-09-11 13:44:43 -0300

    add one exercise

---
+++ exercises/exer11.py
@@ -0,0 +1,11 @@
+#desafio: printar 'Isaque', porém, tendo duas variáveis com valor 'e', colocando ambas no codigo, mas deixando somente uma no print
+
+i='i'
+s='S'
+a='A'
+q='q'
+u='u'
+e='e'
+ee='e'
+
+print(f'{i.upper()}{(s+a+q+u+e+ee).lower()}'[0:6])
